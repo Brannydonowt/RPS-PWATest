7e3041db6ef17d8db80a72d51696a5fd942c3437
BrandonRPS <brandon.sidebottom@rpssoftware.co.uk>
2022-06-09 12:02:00 +0100

    Disabled Post

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "RPS Software-RPSWebAppTutorial-0.1.0";
+const contentToCache = [
+    "Build/6 - No Post.loader.js",
+    "Build/6 - No Post.framework.js",
+    "Build/6 - No Post.data",
+    "Build/6 - No Post.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
